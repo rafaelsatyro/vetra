a9f1b4034c01830336ab7cb4b601b14318708f9a
rafaelsatyro <rafael.satyro@hotmail.com>
2024-12-01 02:21:57 -0300

    inserção de componentes faltantes

---
+++ src/Components/Pages/Services/Services.tsx
@@ -0,0 +1,15 @@
+import './Styles.css'
+
+function Services() {
+  return (
+    <>
+      <div className="container-services">   
+        Services
+      </div>
+    </>
+  );
+}
+
+export default Services;
+
+
---
+++ src/Components/Pages/SucessStories/SucessStories.tsx
@@ -0,0 +1,15 @@
+import './Styles.css'
+
+function SucessStories() {
+  return (
+    <>
+      <div className="container-sucess-stories">   
+        Sucess Stories
+      </div>
+    </>
+  );
+}
+
+export default SucessStories;
+
+
